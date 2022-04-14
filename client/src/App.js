1b8b13747fb79391fda2a5c961c584f2b54b757f
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2022-04-15 02:00:04 +0900

    client tnwjd

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,25 @@
-import logo from './logo.svg'
-import './App.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [list, setList] = useState([])
-
-  useEffect(() => {
-    ;(async () => {
-      const { data: resultList } = await axios.get('/api/list')
-      setList(resultList)
-    })()
-  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.!Q!!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
-        <div>List</div>
-        {list.map((item, idx) => (
-          <div key={idx}>{`${item.id}/${item.name}`}</div>
-        ))}
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
